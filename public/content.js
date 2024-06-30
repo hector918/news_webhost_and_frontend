cc8899a6c6b0d2c8282cfddc6e7e18befbe9353d
hector918 <hector_zhong@outlook.com>
2024-06-30 09:13:35 -0400

    updating frontend js with routing

--- public/content.js
+++ public/content.js
@@ -1,24 +1,77 @@
 import frame from './frame.js';
-
-
-const spa_base = new frame.baseH({
-  name: "i am the app base",
-  structure: `
-    <section class="hero is-primary">
-      <div class="hero-body">
-        <p class="title" id_="testing">Primary hero</p>
-        <p class="subtitle">Primary subtitle</p>
-      </div>
-    </section>
-  `,
+const elementRoot = frame.elementRoot;
+new frame.baseH({
+  name: "root",
+  structure: `<div id_="root_div"></div>`,
   parent: document.querySelector("body"),
+});
+console.log(elementRoot)
+elementRoot.setRoute("/testing/game.index?variable='hello world&abcd=e231/kkk", () => {
+  const testing = new frame.baseH({
+    name: "testing",
+    structure: `<div id_="testing_">testing</div>`,
+    parent: elementRoot.elementList["root"].elements["root_div"],
+  });
+  const gameIndex = new frame.baseH({
+    name: "game.index",
+    structure: `<div id_="game.index">game.index</div>`,
+    parent: testing.elements["testing_"],
+  });
+  const kkk = new frame.baseH({
+    name: "kkk",
+    structure: `<div id_="kkk">kkk</div>`,
+    parent: gameIndex.elements["game.index"],
+  });
+})
+
+elementRoot.goRoute("/testing/game.index?variable='hello world&abcd=e231/kkk");
+
+//
+let navbar = new frame.baseH({
+  name: "navbar",
+  structure: `
+    <nav class="navbar is-fixed-top is-link" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand is-flex-grow-1">
+      <a class="navbar-item" href="https://bulma.io">
+        <svg width="140" height="160" viewBox="0 0 140 160" fill="none" xmlns="http://www.w3.org/2000/svg">
+        
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M0 110L10 40L50 0L100 50L70 80L110 120L50 160L0 110Z" fill="#00D1B2"/>
+      </svg>
+
+      </a>
+      <div class="navbar-item is-justify-content-space-around is-flex-grow-1">
+        <a class="navbar-item">testing</a>
+        <a class="navbar-item">testing</a>
+        <a class="navbar-item">testing</a>
+        <a class="navbar-item">testing</a>
+        
+      </div>
+      
+    </div>
+  </nav>
+  `,
+  parent: elementRoot.get("root").elements['root_div'],
   render: {
     testing: function (str) {
       this.elements["testing"].innerHTML = str;
     }
   }
-
 });
 
+const user_info = new frame.varH();
+// user_info.onChangeCall({ component: spa_base, renderHandle: "testing" });
 
-export { spa_base }
+user_info.set("hello world");
+user_info.set("hello world1");
+user_info.set("hello world2");
+
+
+
+export { navbar }
+
+//event
+
+window.addEventListener('resize', frame.throttle(function (event) {
+  // Your code here
+  console.log('Window resized to: ' + window.innerWidth + 'x' + window.innerHeight);
+}, 500));
--- public/frame.js
+++ public/frame.js
@@ -1,68 +1,322 @@
-class varH {
-  updateList = []
+const compoent_name_prefix = "compoent_name_";
+//////////////////////////////////////////////////////
+class elementRootH {
+  elementList = {}
+  routes = {}
   constructor() {
 
   }
-  get() {
+  setRoute(path, document) {
+    const queryStrings = parseHash(path);
+    const temp = {};
+    let currentPath = temp;
+    for (let idx = 0; idx < queryStrings.length; idx++) {
+      const itm = queryStrings[idx];
+
+      const { path, params } = parseNestedQueryString(itm);
+      if (path === "_") throw new Error("path '_' not allow.");
+      currentPath[path] = {
+        "_": {
+          params,
+          next: {}
+        }
+      };
+
+      if (idx === queryStrings.length - 2) {
+        currentPath[path]["_"]['function'] = document;
+      }
+      currentPath = currentPath[path];
+    }
+    this.routes = { ...this.routes, ...temp };
+  }
+  goRoute(path) {
+    const queryStrings = parseHash(path);
+    const fullRoute = {};
+    let currentPath = this.routes;
+    for (let idx = 0; idx < queryStrings.length; idx++) {
+      const itm = queryStrings[idx];
+      const { path, params } = parseNestedQueryString(itm);
+
+      currentPath = currentPath[path];
+      if (currentPath['_']['function']) currentPath['_']['function']();
+
+    }
 
   }
-  set() {
-
+  add(name, ele) {
+    if (!this.has(name)) this.elementList[name] = ele;
   }
-  onChangeCall(base) {
-
+  del(name) {
+    if (this.has(name)) {
+      const deleteList = this.elementList[name].DOM.querySelectorAll(compoent_name_prefix);
+      for (let el of deleteList) {
+        const eleName = el.getAttribute(compoent_name_prefix);
+        if (this.has(eleName)) {
+          this.has(eleName).destory();
+          delete this.elementList[eleName];
+        }
+      }
+      delete this.elementList[name];
+    }
+  }
+  get(name) {
+    if (this.has(name)) {
+      return this.elementList[name];
+    } else {
+      return undefined;
+    }
+  }
+  has(name) {
+    return this.elementList[name] !== undefined;
   }
 }
+const elementRoot = new elementRootH();
+
+//////////////////////////////
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+
+class LinkedListQueue {
+  constructor() {
+    this.front = null;
+    this.rear = null;
+    this.size = 0;
+  }
+
+  // 向队列添加元素
+  enqueue(element) {
+    const newNode = new Node(element);
+    if (this.rear) {
+      this.rear.next = newNode;
+    }
+    this.rear = newNode;
+    if (!this.front) {
+      this.front = newNode;
+    }
+    this.size++;
+  }
+
+  // 从队列移除元素
+  dequeue() {
+    if (this.isEmpty()) {
+      return "Queue is empty";
+    }
+    const removedElement = this.front.element;
+    this.front = this.front.next;
+    if (!this.front) {
+      this.rear = null;
+    }
+    this.size--;
+    return removedElement;
+  }
+
+  // 查看队列头部元素
+  frontElement() {
+    if (this.isEmpty()) {
+      return "Queue is empty";
+    }
+    return this.front.element;
+  }
+
+  // 检查队列是否为空
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  // 查看队列大小
+  getSize() {
+    return this.size;
+  }
+
+  // 清空队列
+  clear() {
+    this.front = null;
+    this.rear = null;
+    this.size = 0;
+  }
+}
+//////////////////////////////////////////////////////
+class varH {
+  updateList = {}
+  data = null
+
+  constructor() {
+
+  }
+
+  get() {
+    return this.data;
+  }
+
+  set(data) {
+    const previousData = this.data;
+    this.data = data;
+
+    //update compoents
+    for (let key in this.updateList) {
+      const { component, renderHandle } = this.updateList[key];
+
+      if (component && component.renders && component.renders[renderHandle] !== undefined) {
+        component.renders[renderHandle](data);
+      } else {
+        delete this.updateList[key];
+      }
+    }
+    return previousData;
+  }
+  onChangeCall({ component, renderHandle }) {
+    this.updateList[Object.values(this.updateList).length] = { component, renderHandle };
+  }
+}
+//////////////////////////////////////////////////////
 class baseH {
-  variable = {}
   elements = {}
   renders = {}
-  constructor({ name, structure, parent, variable, render }) {
+
+  constructor({ name, structure, parent, render }) {
+    if (elementRoot.has(name)) throw "component name already existed.";
+    elementRoot.add(name, this);
     this.name = name;
     this.parent = parent;
     this.processStructure(structure);
 
-    for (let itm in variable) {
-      this.variable[itm] = variable[itm];
-    }
     for (let itm in render) {
       this.renders[itm] = render[itm].bind(this);
     }
-    // parent.append(this.DOM)
+
   }
+
   render(obj) {
     for (let itm in obj) {
-      this.variable[itm] = obj[itm];
-      console.log(this.renders[itm])
       if (this.renders[itm]) this.renders[itm](obj[itm]);
     }
   }
 
   processStructure(structure) {
     const parser = new DOMParser();
-    this.DOM = parser.parseFromString(structure, "text/html");
-    const elements = this.DOM.querySelectorAll('[id_]');
-    this.parent.append(this.DOM.body.firstElementChild);
-
+    const dom = parser.parseFromString(structure, "text/html");
+    this.DOM = dom.body.firstElementChild;
+    const elements = dom.querySelectorAll('[id_]');
+    this.parent.append(this.DOM);
+    this.DOM.setAttribute(compoent_name_prefix, this.name);
     for (let el of elements) {
       this.elements[el.getAttribute("id_")] = el;
     }
+  }
 
+  destory() {
+    for (let itm in this.renders) this.renders[itm] = null;
+
+    const descendants = document.querySelectorAll(`[${compoent_name_prefix}]`);
+    for (let el of descendants) {
+      // elementRoot[el.getAttribute(compoent_name_prefix)].destory();
+    }
+    this.renders = null;
+    this.DOM.remove();
   }
 }
 
-//通用函数
+//通用函数//////////////////////////////////////////////////
+// function parseURL(url) {
+//   const result = {};
+
+//   try {
+//     const urlObj = new URL(url);
+//     result.protocol = urlObj.protocol;
+//     result.hostname = urlObj.hostname;
+//     result.port = urlObj.port;
+//     result.pathname = urlObj.pathname;
+//     result.search = urlObj.search;
+//     result.hash = parseHash(urlObj.hash);
+//     result.username = urlObj.username;
+//     result.password = urlObj.password;
+//     result.origin = urlObj.origin;
+//     result.host = urlObj.host;
+//   } catch (error) {
+//     throw new Error('Invalid URL');
+//   }
+
+//   return result;
+// }
+function parseNestedQueryString(queryString) {
+  if (!queryString) return null;
+  const queryParamsIndex = queryString.indexOf("?");
+  if (queryParamsIndex !== -1) {
+    const ret = { path: queryString.slice(0, queryParamsIndex), params: {} };
+
+    const params = queryString.slice(queryParamsIndex + 1);
+    for (let param of params.split("&")) {
+      const [key, value] = param.split("=");
+      ret.params[key] = value;
+    }
+    return ret;
+  } else {
+    return { path: queryString };
+  }
+}
+
+function parseHash(hash) {
+  if (!hash) return null;
+  // 去掉前面的/，然后按/分割
+  const hashLevels = hash.slice(1).split('/');
+  return hashLevels;
+}
+function getAncestors(element, attribute = compoent_name_prefix) {
+  let ancestors = [];
+  while (element) {
+    ancestors.push(element);
+    if (element.getAttribute(attribute) !== undefined) {
+      break;
+    }
+    element = element.parentElement;
+  }
+  return ancestors;
+}
+function getDescendants(element, conditionFn) {
+  let descendants = [];
+
+  function traverse(node) {
+    let children = node.children;
+    for (let i = 0; i < children.length; i++) {
+      if (typeof conditionFn === "function") {
+        if (conditionFn(children[i])) {
+          descendants.push(children[i]);
+        }
+      }
+      traverse(children[i]);
+    }
+  }
+
+  traverse(element);
+  return descendants;
+}
 //动态加载css
 async function loadStyles(stylesheets, parent = document.head) {
   if (parent === undefined) return false;
-  let arr = await Promise.all(stylesheets.map(url => fetch(url)))
-  arr = await Promise.all(arr.map(url => url.text()))
-  const style = document.createElement('style')
+  let arr = await Promise.all(stylesheets.map(url => fetch(url)));
+  arr = await Promise.all(arr.map(url => url.text()));
+  const style = document.createElement('style');
   style.textContent = arr.reduce(
     (prev, fileContents) => prev + fileContents, ''
   )
   parent.appendChild(style);
 }
+//
+function throttle(fn, wait) {
+  let lastCall = Date.now() - wait;
+  return function () {
+    let now = Date.now();
+    if (now - lastCall >= wait) {
+      lastCall = now;
+      fn.apply(this, arguments);
+    }
+  };
+}
+
 /////非阻塞渲染间任务
 function _runTask(task, callback) {
   let start = Date.now();
@@ -87,16 +341,12 @@ window.onload = function () {
     console.log("This device is using a mouse or other non-touch input.");
   }
 };
+var isMobile = /iPhone|iPod|iPad|Android|BlackBerry/.test(navigator.userAgent);
 function vibrate() {
   if (isMobile) {
     navigator.vibrate(50);
   }
 }
 //////////
-const dateRoot = {};
 
-dateRoot['test'] = new varH();
-
-
-
-export default { baseH, varH, dateRoot }
+export default { baseH, varH, LinkedListQueue, elementRoot, throttle }
